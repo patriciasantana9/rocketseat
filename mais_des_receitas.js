a9b2b74045da3f0ef589a134728a20c02ab6d0d5
Patrícia Santana <patriciasan3@gmail.com>
2023-03-20 19:40:15 -0300

    conclusão

--- mais_des_receitas.js
+++ mais_des_receitas.js
@@ -1,3 +1,5 @@
+let div = document.querySelector(".div");
+
 let familia = {
     incomes: [250, 587, 786, 1000],
     expenses: [70, 75, 50, 29.56]
@@ -10,27 +12,25 @@ function sum(array) {
     for (let value of array){
         total += value;  //total = total + value
     }
-
     return total;
 }
-
 function calculateBalance() {
     //calcular entradas
     const calculateIncome = sum(familia.incomes)
     //calcular saídas
     const calculateExpenses = sum(familia.expenses)
+    //calcular a diferença
+    let total = calculateIncome - calculateExpenses;
 
-    const total = calculateIncome - calculateExpenses;
+    let bom = total >= 0;
 
-    const bom = total >= 0;
-
-    const balanceTxt = "";
+    let balanceTxt = "";
     
     if (bom) {
         balanceTxt = "positivo";
     } else {
         balanceTxt = "negativo"
-    }
-
-    console.log(`Seu saldo é: ${balanceTxt}`)
+    }   
+    div.innerHTML = `Seu saldo é: ${balanceTxt}`;
 }
+calculateBalance();
