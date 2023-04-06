22b7b1f97268367e9a7fb830301ed65908235bad
Patrícia Santana <patriciasan3@gmail.com>
2023-04-06 20:19:12 -0300

    separação-em-pastas

---
+++ nodejs/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+// import express from 'express';
+
+const app = express();
+app.set("view engine", "ejs")  //a ferramenta usada para renderizar html será o ejs
+
+app.get("/", function(req, res) {       //criação de rota 
+    res.render("index");                //res: resposta: renderizar o index.ejs
+})
+app.get("/sobre", function(req, res) {       //criação de rota 
+    res.render("about");
+})
+
+app.listen(8080);   //ouvidor de porta, ouvir a porta 8080
+// console.log('rodando')
