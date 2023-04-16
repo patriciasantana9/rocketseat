7a6daab201a7602491016d4f34c557440a3f6c8c
Patrícia Santana <patriciasan3@gmail.com>
2023-04-15 21:39:21 -0300

    teste

---
+++ teste/array.js
@@ -0,0 +1,14 @@
+//base
+let frutas = ['manga', 'abacaxi', 'banana', 'maça'];
+
+//map
+let frutas_map = frutas.map(() => {
+
+})
+console.log(frutas_map);
+//filter
+let frutas_filter = frutas.filter();
+console.log(frutas_filter)
+//reduce
+//sort
+//push
